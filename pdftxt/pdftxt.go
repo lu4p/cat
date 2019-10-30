e06d0a84bf0673baf0ce0e7b0d055ac97c3980eb
alastor <micaelalastor@gmail.com>
2019-10-30 10:33:09 +0300

    added pdf processing from memory

--- pdftxt/pdftxt.go
+++ pdftxt/pdftxt.go
@@ -2,11 +2,52 @@
 package pdftxt
 
 import (
+	"bytes"
 	"os"
 
 	"github.com/lu4p/unipdf/v3/extractor"
 	pdf "github.com/lu4p/unipdf/v3/model"
 )
+
+// BytesToStr prints out contents of a []byte representation of PDF file to stdout.
+func BytesToStr(data []byte) (string, error) {
+	reader := bytes.NewReader(data)
+
+	pdfReader, err := pdf.NewPdfReader(reader)
+	if err != nil {
+		return "", err
+	}
+
+	numPages, err := pdfReader.GetNumPages()
+	if err != nil {
+		return "", err
+	}
+
+	var out string
+
+	for i := 0; i < numPages; i++ {
+		pageNum := i + 1
+
+		page, err := pdfReader.GetPage(pageNum)
+		if err != nil {
+			return "", err
+		}
+
+		ex, err := extractor.New(page)
+		if err != nil {
+			return "", err
+		}
+
+		text, err := ex.ExtractText()
+		if err != nil {
+			return "", err
+		}
+
+		out += text + "\n\n"
+	}
+
+	return out, nil
+}
 
 // ToStr prints out contents of PDF file to stdout.
 func ToStr(inputPath string) (string, error) {
--- pdftxt/pdftxt_test.go
+++ pdftxt/pdftxt_test.go
@@ -1,6 +1,8 @@
 package pdftxt_test
 
 import (
+	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 
@@ -12,6 +14,23 @@ condimentum dolor vitae dictum rutrum. Etiam viverra sit amet mi at lacinia.`
 
 func TestToStr(t *testing.T) {
 	txt, err := pdftxt.ToStr("../test/test.pdf")
+	txt = strings.TrimSpace(txt)
+	if err != nil {
+		t.Error(".pdf failed:", err)
+	} else if txt == test {
+		t.Log(".pdf success")
+	} else {
+		t.Error(".pdf does not match test:", txt, test)
+	}
+}
+
+func TestBytesToStr(t *testing.T) {
+	file, err := os.Open("../test/test.pdf")
+	if err != nil {
+		t.Error("can't open test.pdf file")
+	}
+	data, err := ioutil.ReadAll(file)
+	txt, err := pdftxt.BytesToStr(data)
 	txt = strings.TrimSpace(txt)
 	if err != nil {
 		t.Error(".pdf failed:", err)
